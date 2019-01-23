6d9ed26d5abce7a94a6424dd4822460921e5e322
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-23 15:51:40 -0700

    adds routes for merchants: index, show. adds all merchant biz intelligience routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show]
+      namespace :merchants do
+        get '/revenue', to: "business_data#revenue", as: "merchants_revenue"
+        get '/:id/revenue', to: "business_data#revenue", as: "merchant_revenue"
+        get '/most_revenue', to: "business_data#most_revenue", as: "most_revenue"
+        get '/most_items', to: "business_data#most_items", as: "most_items"
+      end
+    end
+  end
 end
