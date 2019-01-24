a07a1c14b29db99acfe7f95112948f0b9a218e43
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-23 18:02:25 -0700

    adds Merchant.most_items

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,4 +13,16 @@ class Merchant < ApplicationRecord
     .order("revenue DESC")
     .limit(x)
   end
+
+  def self.most_items(x)
+    joins(
+      "INNER JOIN invoices ON invoices.merchant_id=merchants.id
+       INNER JOIN invoice_items ON invoice_items.invoice_id=invoices.id
+       INNER JOIN transactions ON transactions.invoice_id=invoices.id")
+    .select("merchants.*, SUM(invoice_items.quantity) AS items_sold")
+    .where("transactions.result = ?", 'success')
+    .group(:id)
+    .order("items_sold DESC")
+    .limit(x)
+  end
 end
