4122c94d4a127d4a77620a4fb453ff0321a66200
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-24 15:22:21 -0700

    adds using serializer for merchants-items

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   def index
     merchant = Merchant.find(params[:id])
-    
-  end
-
-  def show
-
+    render json: ItemSerializer.new(merchant.items)
   end
 end
