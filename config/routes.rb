3f3a97bc34d8faed164ec38bddc28eba9f090a0d
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-24 14:27:39 -0700

    adds merchant biz intell routes and some relationship routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
       namespace :merchants do
-        get '/revenue', to: "business_data#revenue", as: "merchants_revenue"
-        get '/:id/revenue', to: "business_data#revenue", as: "merchant_revenue"
-        get '/most_revenue', to: "business_data#most_revenue", as: "most_revenue"
-        get '/most_items', to: "business_data#most_items", as: "most_items"
+        get '/:id/items', to: "items#index", as: "items"
+        get '/:id/invoices', to: "invoices#index", as: "invoices"
+        get '/revenue', to: "revenues#index", as: "revenues"
+        get '/:id/revenue', to: "revenues#show", as: "revenue"
+        get '/most_revenue', to: "most_revenue#index", as: "most_revenue"
+        get '/most_items', to: "most_items#index", as: "most_items"
       end
     end
   end
