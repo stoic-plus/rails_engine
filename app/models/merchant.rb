c34be2c9760e672d9c1f5cecb4283438c34a6532
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-23 17:49:20 -0700

    adds Merchant.most_revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,16 @@
 class Merchant < ApplicationRecord
+  has_many :items
+  has_many :invoices
+
+  def self.most_revenue(x)
+    joins(
+      "INNER JOIN invoices ON invoices.merchant_id=merchants.id
+       INNER JOIN invoice_items ON invoice_items.invoice_id=invoices.id
+       INNER JOIN transactions ON transactions.invoice_id=invoices.id")
+    .select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .where("transactions.result = ?", 'success')
+    .group(:id)
+    .order("revenue DESC")
+    .limit(x)
+  end
 end
