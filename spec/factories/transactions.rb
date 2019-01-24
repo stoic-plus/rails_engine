15dd398a04cfcb74750ad5c7edbf5fbd55319705
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-23 17:19:50 -0700

    changes result to transaction - adds failed transaction

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
     invoice { nil }
     credit_card_number { 1 }
     credit_card_expiration_date { "2019-01-23 11:51:54" }
-    result { "MyString" }
+    result { "success" }
+  end
+  factory :failed_transaction, parent: :transaction do
+    result { "failed" }
   end
 end
