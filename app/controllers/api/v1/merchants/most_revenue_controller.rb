4de61575bca0d7797cf5a42ab55268a5eb8a0e11
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-25 16:42:46 -0700

    adds MerchantSerializer and using it in most_rev_contr

--- app/controllers/api/v1/merchants/most_revenue_controller.rb
+++ app/controllers/api/v1/merchants/most_revenue_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Merchants::MostRevenueController < ApplicationController
   def index
     merchants = Merchant.most_revenue(params[:quantity])
-    render json: MerchantSerializer(Merchant)
+    render json: MerchantSerializer.new(merchants)
   end
 end
