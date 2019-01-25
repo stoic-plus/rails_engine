bfb3e990158e6e3b1d5b88cd858989040998e19c
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-24 18:39:41 -0700

    adds more random expiration date to transaction factory

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :transaction do
     invoice { nil }
     credit_card_number { rand(1000000000000000) }
-    credit_card_expiration_date { Time.now }
+    credit_card_expiration_date { Time.at(rand(1000000000)) }
     result { "success" }
   end
   factory :failed_transaction, parent: :transaction do
