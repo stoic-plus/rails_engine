036c3fb39bd2574bdc1264a804dfaac974dbe6e6
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-25 12:36:02 -0700

    moves revenue_by_date from Merchant to Invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,4 +5,12 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.revenue_by_date(date_time)
+    joins(:invoice_items, :transactions)
+     .select("SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+     .where("transactions.result = ? AND DATE_TRUNC('day', invoices.updated_at) = ?", 'success', date_time.to_date)
+     .group("DATE_TRUNC('day', invoices.updated_at)")[0]
+     .revenue
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -19,12 +19,4 @@ class Merchant < ApplicationRecord
     .order("items_sold DESC")
     .limit(x)
   end
-
-  def self.revenue_by_date(date_time)
-   Invoice.joins(:invoice_items, :transactions)
-    .select("SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue")
-    .where("transactions.result = ? AND DATE_TRUNC('day', invoices.updated_at) = ?", 'success', date_time.to_date)
-    .group("DATE_TRUNC('day', invoices.updated_at)")[0]
-    .revenue
-  end
 end
