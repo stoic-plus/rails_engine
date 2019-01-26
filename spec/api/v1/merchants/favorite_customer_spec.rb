dd9eb468b2db1f40c46a0cce0d647ab727cb1be1
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 14:30:24 -0700

    adds spec for merchants/:id/favorite_customer endpoint

---
+++ spec/api/v1/merchants/favorite_customer_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'Merchant - Favorite Customer' do
+  it 'returns customer with most successful transactions' do
+    c_1 = create(:customer)
+    c_2 = create(:customer)
+    m = create(:merchant)
+
+    i = create(:invoice, customer: c_1, merchant: m, updated_at: 2.days.ago)
+    t = create(:transaction, invoice: i, result: 'success')
+
+    i_1 = create(:invoice, customer: c_1, merchant: m, updated_at: 2.days.ago)
+    t_1 = create(:failed_transaction, invoice: i_1)
+
+    i_2 = create(:invoice, customer: c_2, merchant: m, updated_at: 2.days.ago)
+    t_2 = create(:transaction, invoice: i_2, result: 'success')
+
+    i_3 = create(:invoice, customer: c_2, merchant: m, updated_at: 1.days.ago)
+    t_3 = create(:transaction, invoice: i_3, result: 'success')
+
+    get "/api/v1/merchants/#{m.id}/favorite_customer"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)["data"]
+    expect(json["type"]).to eq("favorite_customer")
+    expect(json["attributes"]["id"]).to eq(c_2.id)
+    expect(json["attributes"]["first_name"]).to eq(c_2.first_name)
+    expect(json["attributes"]["last_name"]).to eq(c_2.last_name)
+  end
+end
