1baa11b5e83dc14a00b452bac07de26035f7c982
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 15:18:19 -0700

    changes expected json[type] and adds sad path testing if no customer found

--- spec/api/v1/merchants/favorite_customer_spec.rb
+++ spec/api/v1/merchants/favorite_customer_spec.rb
@@ -5,6 +5,7 @@ describe 'Merchant - Favorite Customer' do
     c_1 = create(:customer)
     c_2 = create(:customer)
     m = create(:merchant)
+    m_2 = create(:merchant)
 
     i = create(:invoice, customer: c_1, merchant: m, updated_at: 2.days.ago)
     t = create(:transaction, invoice: i, result: 'success')
@@ -23,10 +24,17 @@ describe 'Merchant - Favorite Customer' do
     expect(response).to be_successful
 
     json = JSON.parse(response.body)["data"]
-    expect(json["type"]).to eq("favorite_customer")
+    expect(json["type"]).to eq("customer")
     expect(json["attributes"]["id"]).to eq(c_2.id)
     expect(json["attributes"]["first_name"]).to eq(c_2.first_name)
     expect(json["attributes"]["last_name"]).to eq(c_2.last_name)
     expect(json["attributes"]["transaction_count"]).to eq(2)
+
+    get "/api/v1/merchants/#{m_2.id}/favorite_customer"
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body)["data"]
+    expect(json["type"]).to eq("api_error")
+    expect(json["attributes"]["message"]).to eq("No customer found")
   end
 end
