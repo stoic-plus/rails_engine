268a49d89d79d7db64a0a7cc1f6185023a4bc90f
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 13:08:43 -0700

    adds BusinessData module to hold helper classes for biz intel endpoints

---
+++ app/helpers/business_data.rb
@@ -0,0 +1,31 @@
+module BusinessData
+  class TotalRevenueForDate
+    @@count = 0
+    attr_reader :total_revenue, :date, :id
+    def initialize(total_revenue, date)
+      @id = @@count
+      @@count += 1
+      @total_revenue = total_revenue
+      @date = date
+    end
+  end
+  class TotalRevenue
+    @@count = 0
+    attr_reader :total_revenue, :id
+    def initialize(total_revenue, date)
+      @id = @@count
+      @@count += 1
+      @total_revenue = total_revenue
+    end
+  end
+  class ApiError
+    @@count = 0
+    attr_reader :message, :date, :id
+    def initialize(message, date)
+      @id = @@count
+      @@count += 1
+      @message = message
+      @date = date
+    end
+  end
+end
