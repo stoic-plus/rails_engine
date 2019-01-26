e2146af1f9b76ffe420a2a349c2e0b78611859ec
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 13:08:01 -0700

    adds revenues controller for merchants namespace

---
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::Merchants::RevenuesController < ApplicationController
+  include BusinessData
+  def index
+    total_revenue = Invoice.total_revenue_by_date(params[:date])
+    if total_revenue
+      render json: TotalRevenueForDateSerializer.new(BusinessData::TotalRevenueForDate.new(total_revenue, params[:date]))
+    else
+      render json: ApiErrorSerializer.new(BusinessData::ApiError.new("No results found for that date", params[:date]))
+    end
+  end
+
+  def show
+
+  end
+end
