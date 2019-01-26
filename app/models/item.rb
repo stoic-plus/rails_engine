241116e7c6e31e1e57768f8066fe991d31e851e7
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 16:40:44 -0700

    adds Item.top_items_by_quant_sold

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,15 @@ class Item < ApplicationRecord
     .order("revenue DESC")
     .limit(num)
   end
+
+  def self.top_items_by_quantity_sold(num=nil)
+    joins("INNER JOIN invoice_items ON items.id=invoice_items.item_id
+           INNER JOIN invoices ON invoice_items.invoice_id=invoices.id
+           INNER JOIN transactions ON invoices.id=transactions.invoice_id")
+    .select("items.*, SUM(invoice_items.quantity) AS quantity_sold")
+    .where("transactions.result = ?", 'success')
+    .group(:id)
+    .order("quantity_sold DESC")
+    .limit(num)
+  end
 end
