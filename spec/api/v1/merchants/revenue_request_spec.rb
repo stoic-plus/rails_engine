c3ac3266f54d9bd337b89255d2947978008fd857
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 13:57:36 -0700

    changes testing for no results found for date - adds it for merchants/:id/revenue

--- spec/api/v1/merchants/revenue_request_spec.rb
+++ spec/api/v1/merchants/revenue_request_spec.rb
@@ -11,6 +11,7 @@ describe 'Merchants - Revenue' do
     item_3 = create(:item, merchant: @m_3)
     days2_ago = 2.days.ago
     day1_ago = 1.days.ago
+    days4_ago = 4.days.ago
 
     i = create(:invoice, customer: c, merchant: @m, updated_at: days2_ago)
     ii = create(:invoice_item, item: item, invoice: i, quantity: 2, unit_price: 2)
@@ -46,14 +47,12 @@ describe 'Merchants - Revenue' do
     expect(total_rev["attributes"]["total_revenue"]).to eq(4)
     expect(total_rev["attributes"]["date"]).to eq(day1_ago.to_s)
 
-    # get "/api/v1/merchants/revenue?date=#{4.days.ago}"
-    #
-    # expect(response).to be_successful
-    # response = JSON.parse(response.body)["data"]
-    # expect(response).to eq({
-    #   message: "No results found for date",
-    #   date: 4.days.ago.to_s
-    # })
+    get "/api/v1/merchants/revenue?date=#{days4_ago}"
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body)["data"]
+    expect(json["attributes"]["message"]).to eq("No results found for that date")
+    expect(json["attributes"]["date"]).to eq(days4_ago.to_s)
   end
 
   it 'returns total revenue for one merchant on specific invoice date' do
@@ -90,5 +89,33 @@ describe 'Merchants - Revenue' do
     total_revenue_for_date = JSON.parse(response.body)["data"]
     expect(total_revenue_for_date["attributes"]["total_revenue"]).to eq(4)
     expect(total_revenue_for_date["attributes"]["date"]).to eq(day1_ago.to_s)
+  end
+
+  it 'returns total_revenue for a merchant - no date' do
+    c = create(:customer)
+    @m_2 = create(:merchant)
+    item_2 = create(:item, merchant: @m_2)
+    days2_ago = 2.days.ago
+    day1_ago = 1.days.ago
+
+    i = create(:invoice, customer: c, merchant: @m_2, updated_at: days2_ago)
+    ii = create(:invoice_item, item: item_2, invoice: i, quantity: 2, unit_price: 2)
+    t = create(:transaction, invoice: i, result: 'success')
+
+    i_2 = create(:invoice, customer: c, merchant: @m_2, updated_at: days2_ago)
+    ii_2 = create(:invoice_item, item: item_2, invoice: i_2, quantity: 3, unit_price: 5)
+    t_2 = create(:transaction, invoice: i_2, result: 'success')
+
+    i_3 = create(:invoice, customer: c, merchant: @m_2, updated_at: day1_ago)
+    ii_3 = create(:invoice_item, item: item_2, invoice: i_3, quantity: 4, unit_price: 1)
+    t_3 = create(:transaction, invoice: i_3, result: 'success')
+
+    get "/api/v1/merchants/#{@m_2.id}/revenue"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body)["data"]
+    expect(json["type"]).to eq("total_revenue")
+    expect(json["attributes"]["total_revenue"]).to eq(23)
   end
 end
