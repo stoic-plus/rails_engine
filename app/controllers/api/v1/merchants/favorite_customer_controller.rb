b48db4d155d5732401bd19783ca7a28defae431d
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 15:19:41 -0700

    adds calling Customer method and sad path

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
   def index
-    merchant = Merchant.find(params[:id])
-    render json: CustomerSerializer.new(merchant.favorite_customer)
+    customer_info = Customer.favorite_customer_for(params[:id])
+    if customer_info
+      render json: CustomerSerializer.new(customer_info)
+    else
+      render json: ApiErrorSerializer.new(BusinessData::ApiError.new("No customer found", DateTime.now))
+    end
   end
 end
