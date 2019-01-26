86df80ffe0c72ddef1559347821a5d30e77f2e30
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 15:20:03 -0700

    adds selecting the transaction_count

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,10 @@ class Customer < ApplicationRecord
 
   def self.favorite_customer_for(merchant_id, num=1)
     joins(invoices: [:transactions])
+      .select("customers.*, COUNT(transactions.id) AS transaction_count")
       .where("transactions.result = ? AND invoices.merchant_id = ?", 'success', merchant_id)
       .group(:id)
-      .order("COUNT(transactions.id) AS transaction_count")
-      .limit(num)
+      .order("transaction_count DESC")
+      .limit(num)[0]
   end
 end
