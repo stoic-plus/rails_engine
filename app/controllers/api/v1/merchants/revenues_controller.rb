8e7d790c38670d9b4498d9c7b693a1901e96c52d
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-26 13:37:10 -0700

    adds show route to merchants/revenues controller

--- app/controllers/api/v1/merchants/revenues_controller.rb
+++ app/controllers/api/v1/merchants/revenues_controller.rb
@@ -10,6 +10,12 @@ class Api::V1::Merchants::RevenuesController < ApplicationController
   end
 
   def show
-
+    invoices = Merchant.find(params[:id]).invoices
+    total_revenue = invoices.total_revenue_by_date(params[:date])
+    if total_revenue
+      render json: TotalRevenueForDateSerializer.new(BusinessData::TotalRevenueForDate.new(total_revenue, params[:date]))
+    else
+      render json: ApiErrorSerializer.new(BusinessData::ApiError.new("No results found for that date", params[:date]))
+    end
   end
 end
