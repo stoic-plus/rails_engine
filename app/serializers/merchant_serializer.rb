608780d372673b9f171ae9d883f1e1c625fa34be
stoic-plus <ricardoledesmadev@gmail.com>
2019-01-27 10:09:39 -0700

    unable to serialize transaction count

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,4 +1,5 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name
+  # attribute :transaction_count, if: Proc.new {|record| record.transaction_count.present? }
 end
